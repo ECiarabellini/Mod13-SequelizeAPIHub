88c2bd040d927d3b0bf15093de39d4686a0bc1ad
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-02-29 22:23:11 -0800

    Add all tag-routes to tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -36,12 +36,36 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    // Find the tag by ID in the request parameters
+    const tag = await Tag.findByPk(req.params.id);
+
+    // If tag with the given ID does not exist, return 404 Not Found
+    if (!tag) {
+      return res.status(404).json({ error: 'Tag not found' });
+    }
+
+    // Update the tag's name with the value from request body
+    await tag.update({ tag_name: req.body.tag_name });
+    //return new tag if successful
+    res.status(200).json(tag);
+  } catch (error) {
+    // If an error occurs, return 500 Internal Server Error
+    console.error('Error updating tag:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -53,6 +77,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
 
+    // If tag with the given ID does not exist, return 404 Not Found
     if (!tagData) {
       res.status(404).json({ message: 'No tag with that id!' });
       return;
